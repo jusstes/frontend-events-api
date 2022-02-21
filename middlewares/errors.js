9c92faf80ff7909977a348e1ab289129ffe0e954
Aleksey Golodnikov <jusstes@yandex.ru>
2022-02-21 22:29:38 +0700

    fix: отдавать error, когда статус ответа не 200

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -4,7 +4,7 @@ module.exports.errorsHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode)
     .send({
-      message: statusCode === 500
+      error: statusCode === 500
         ? MESSAGES.SERVER
         : message,
     });
